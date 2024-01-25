8124f65fc9ada93bf27280f2070627c414ac0a91
SumitBachhav <sumitb2156@gmail.com>
2024-01-25 14:53:15 +0530

    initial setup and using components

---
+++ components/FlatCards.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default function FlatCards(){
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+
+  const styles = StyleSheet.create({})
